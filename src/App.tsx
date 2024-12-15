85bf5f1e4a9fb7ed8f94d6f3362aa50111bd04a9
ankitojha07 <ankitojha1409@gmail.com>
2024-12-15 12:43:16 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="font-bold text-4xl">Kanban dashboard in React</p>
+      <p>react application for a kanban dahsboard</p>
+    </div>
+  );
+};
+
+export default App;
