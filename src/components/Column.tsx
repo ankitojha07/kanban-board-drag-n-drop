356f8620f9181a8f06708fd07241b518ca20ef9a
ankitojha07 <ankitojha1409@gmail.com>
2024-12-24 01:01:12 +0530

    colum delete logic updated

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -9,10 +9,9 @@ interface ColumnDetails {
 
 const Column = ({ col, onDelete }: ColumnDetails) => {
   const [task, setTask] = useState<string[]>([]);
-  const [hoveredTaskIndex, setHoveredTaskIndex] = useState<number | null>(null);
 
   function createNewTask() {
-    setTask((prevTasks) => [...prevTasks, "Task " + (prevTasks.length + 1)]);
+    setTask((prevTasks) => [...prevTasks, `Task ${prevTasks.length + 1}`]);
   }
 
   function deleteTask(index: number) {
@@ -21,6 +20,7 @@ const Column = ({ col, onDelete }: ColumnDetails) => {
 
   return (
     <div className="h-96 w-72 p-2 rounded-md bg-zinc-900 flex flex-col justify-between items-start">
+      {/* Column Header */}
       <div className="w-full flex justify-between text-white items-center bg-zinc-800 p-2">
         <p className="py-1 bg-zinc-900 rounded-full px-3 font-bold">
           {task.length}
@@ -32,29 +32,23 @@ const Column = ({ col, onDelete }: ColumnDetails) => {
         />
       </div>
 
+      {/* Task List */}
       <div className="flex flex-col gap-4 mt-2 w-full overflow-y-auto scrollbar-hide">
         {task.map((taskName, index) => (
-          <div
-            key={index}
-            className="w-full relative"
-            onMouseEnter={() => setHoveredTaskIndex(index)}
-            onMouseLeave={() => setHoveredTaskIndex(null)}
-          >
+          <div key={index} className="w-full relative flex items-center gap-2">
             <textarea
               placeholder={taskName}
               className="w-full p-2 text-white rounded-md resize-none py-2 bg-zinc-800 outline-none scrollbar-hide"
-              rows={2}
             />
-            {hoveredTaskIndex === index && (
-              <MdDelete
-                className="absolute top-2 right-2 text-zinc-600 cursor-pointer text-2xl"
-                onClick={() => deleteTask(index)}
-              />
-            )}
+            <MdDelete
+              className="text-zinc-600 cursor-pointer text-2xl"
+              onClick={() => deleteTask(index)}
+            />
           </div>
         ))}
       </div>
 
+      {/* Add Task Button */}
       <div
         className="flex justify-start items-center p-2 text-white hover:bg-zinc-800 w-full rounded-md hover:text-red-800 cursor-pointer"
         onClick={createNewTask}
